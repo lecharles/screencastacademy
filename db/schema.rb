61b9e66ecc6863a8200a8b8692c0e3b7ee39cd1d
lecharles <charleslozano@gmail.com>
2016-04-10 11:31:21 +0200

    Add Materialize css+js to index view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409142405) do
+ActiveRecord::Schema.define(version: 20160410080650) do
 
   create_table "videos", force: :cascade do |t|
     t.string   "screencast"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160409142405) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "hashtag"
+    t.string   "title"
   end
 
 end
