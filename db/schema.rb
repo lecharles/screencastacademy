f607ca88e6766bde09073a4ee8bfc846f0d4ecbe
lecharles <charleslozano@gmail.com>
2016-04-24 21:35:25 +0200

    Create User mvc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410080650) do
+ActiveRecord::Schema.define(version: 20160424192951) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "videos", force: :cascade do |t|
     t.string   "screencast"
