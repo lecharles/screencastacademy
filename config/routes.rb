d1aa884ce06ad5c8d0ab509b4888a5a66c36b7dd
lecharles <charleslozano@gmail.com>
2016-04-28 20:28:26 +0200

    Add User Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-
-  get 'user_sessions/create'
-
-  get 'user_sessions/destroy'
-
+  root :to => 'users#index'
+  resources :user_sessions
   resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+  
   resources :videos
   get 'pages/about'
 
   get 'pages/contact'
 
-  root 'videos#index'
+  # root 'videos#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
