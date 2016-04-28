8c4d0d40f0877adcae04d2effd1ed8318817d23e
lecharles <charleslozano@gmail.com>
2016-04-28 20:45:22 +0200

    Redirect to Users and Add Notice. Redirect to Login Path & add Alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_login
+
+  private
+  def not_authenticated
+    redirect_to login_path, alert: "Please login first"
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        redirect_to(:users, notice: 'User was successfully created')
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
